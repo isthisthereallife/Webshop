22ce00ace0f5d5640f985491c24dace81fb344f7
JonasHolmkvist <jonas_holmkvist@hotmail.se>
2021-10-25 17:12:13 +0200

    Merge branch 'main' of https://github.com/H0nken/webshop_school_project into css

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
-import Link from "next/link"
-import imagePlaceholder from "../images/imagenotfound.png"
-import { useEffect, useState, useRef } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import imagePlaceholder from "../images/imagenotfound.png";
+import { useEffect, useState, useRef } from "react";
 
 export default function Home({ data }) {
-  const [beers, setBeers] = useState(data)
-  const [searchMode, setSearchMode] = useState("beer_name")
-  const [searchValue, setSearchValue] = useState("")
-  const inputRef = useRef()
+  const [beers, setBeers] = useState(data);
+  const [searchMode, setSearchMode] = useState("beer_name");
+  const [searchValue, setSearchValue] = useState("");
+  const inputRef = useRef();
 
   //körs när nytt val görs i dropdownen
-  const changeSearchMode = ((e) => {
-    setSearchMode(e.target.value)
-  })
+  const changeSearchMode = (e) => {
+    setSearchMode(e.target.value);
+  };
 
   //körs när text i sökrutan ändras
   function handleChangeQuery(event) {
-    setSearchValue(event.target.value)
+    setSearchValue(event.target.value);
   }
 
   useEffect(async () => {
     //gör ny sökning varje gång searchValue eller searchMode ändras
-    const newData = await customSearch(searchMode, searchValue)
-    setBeers(newData)
-  }, [searchValue, searchMode])
+    const newData = await customSearch(searchMode, searchValue);
+    setBeers(newData);
+  }, [searchValue, searchMode]);
 
   return (
     <div className={styles.container}>
@@ -33,9 +33,14 @@ export default function Home({ data }) {
         <title>Brewdog</title>
       </Head>
 
+      <button className="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+        Click me
+      </button>
+
       <main className={styles.main}>
         <h2>SEARCH</h2>
-        <input type="text"
+        <input
+          type="text"
           ref={inputRef}
           placeholder="Search..."
           value={searchValue}
@@ -48,48 +53,48 @@ export default function Home({ data }) {
           <option value="food">Food pairings</option>
         </select>
 
-
         <h1 className={styles.title}>BEERS</h1>
 
         <div className={styles.grid}>
-          {beers && beers.map((beer) => (
-            <div className={styles.card}>
-              <Image
-                unoptimized
-                loader={() => beer.image_url}
-                src={beer.image_url ? beer.image_url : imagePlaceholder}
-                width={100}
-                height={250}
-                alt={beer.name}
-              />
-              <Link key={beer.id} href={`/product/${beer.id.toString()}`}>
-                {beer.name}
-              </Link>
-            </div>
-          ))}
+          {beers &&
+            beers.map((beer) => (
+              <div className={styles.card}>
+                <Image
+                  unoptimized
+                  loader={() => beer.image_url}
+                  src={beer.image_url ? beer.image_url : imagePlaceholder}
+                  width={100}
+                  height={250}
+                  alt={beer.name}
+                />
+                <Link key={beer.id} href={`/product/${beer.id.toString()}`}>
+                  {beer.name}
+                </Link>
+              </div>
+            ))}
         </div>
       </main>
     </div>
-  )
+  );
 }
 //}
 
 async function customSearch(searchType, query) {
-  let res
+  let res;
   if (query.length === 0) {
-    res = await fetch(`https://api.punkapi.com/v2/beers?page1&per_page=80`)
+    res = await fetch(`https://api.punkapi.com/v2/beers?page1&per_page=80`);
+  } else {
+    res = await fetch(
+      `https://api.punkapi.com/v2/beers?${searchType}=${query}`
+    );
   }
-  else {
-    res = await fetch(`https://api.punkapi.com/v2/beers?${searchType}=${query}`)
-  }
-  const data = await res.json()
-  return data
+  const data = await res.json();
+  return data;
 }
 
-
 export async function getStaticProps() {
-  const res = await fetch(`https://api.punkapi.com/v2/beers?page1&per_page=80`)
-  const data = await res.json()
+  const res = await fetch(`https://api.punkapi.com/v2/beers?page1&per_page=80`);
+  const data = await res.json();
 
-  return { props: { data } }
+  return { props: { data } };
 }
