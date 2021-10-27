729cac1d3160221f8b5c21605bd03d4dee4b3cca
Magnus <isthisthereallife@riseup.net>
2021-10-27 13:44:17 +0200

    class => className

--- components/Footer.js
+++ components/Footer.js
@@ -1,47 +1,47 @@
 function Footer() {
   return (
     <div className="footer">
-      <footer class="footer bg-white relative pt-1 border-b-2 border-red-700">
-        <div class="container mx-auto px-6">
-          <div class="sm:flex sm:mt-8">
-            <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
-              <div class="flex flex-col">
-                <span class="font-bold text-red-700 uppercase mb-2">
+      <footer className="footer bg-white relative pt-1 border-b-2 border-red-700">
+        <div className="container mx-auto px-6">
+          <div className="sm:flex sm:mt-8">
+            <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
+              <div className="flex flex-col">
+                <span className="font-bold text-red-700 uppercase mb-2">
                   Footer header 1
                 </span>
-                <span class="my-2">
+                <span className="my-2">
                   <a
                     href="#"
-                    class="text-yellow-700  text-md hover:text-red-500"
+                    className="text-yellow-700  text-md hover:text-red-500"
                   >
                     link 1
                   </a>
                 </span>
-                <span class="my-2">
+                <span className="my-2">
                   <a
                     href="#"
-                    class="text-yellow-700  text-md hover:text-red-500"
+                    className="text-yellow-700  text-md hover:text-red-500"
                   >
                     link 1
                   </a>
                 </span>
-                <span class="my-2">
+                <span className="my-2">
                   <a
                     href="#"
-                    class="text-yellow-700  text-md hover:text-red-500"
+                    className="text-yellow-700  text-md hover:text-red-500"
                   >
                     link 1
                   </a>
                 </span>
               </div>
-              <div class="flex flex-col"></div>
+              <div className="flex flex-col"></div>
             </div>
           </div>
         </div>
-        <div class="container mx-auto px-6">
-          <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
-            <div class="sm:w-2/3 text-center py-6">
-              <p class="text-4xl text-white-400 font-bold mb-2">© BREWDOG</p>
+        <div className="container mx-auto px-6">
+          <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+            <div className="sm:w-2/3 text-center py-6">
+              <p className="text-4xl text-white-400 font-bold mb-2">© BREWDOG</p>
             </div>
           </div>
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,11 @@
 function Navbar() {
   return (
     <>
-      <div class="navContainer">
-        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-16">
-          <div class="flex items-center flex-shrink-0 text-white mr-12 space-x-18">
+      <div className="navContainer">
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-16">
+          <div className="flex items-center flex-shrink-0 text-white mr-12 space-x-18">
             <svg
-              class="fill-current h-12 w-8 mr-9"
+              className="fill-current h-12 w-8 mr-9"
               width="54"
               height="54"
               viewBox="0 0 54 54"
@@ -14,13 +14,13 @@ function Navbar() {
               <path d="#" />
             </svg>
 
-            <span class="font-semibold text-6xl tracking-tight">
+            <span className="font-semibold text-6xl tracking-tight">
               Brewdog Beer
             </span>
           </div>
-          <div class="block lg:hidden">
+          <div className="block lg:hidden">
             <svg
-              class="fill-current h-3 w-3"
+              className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -28,23 +28,23 @@ function Navbar() {
               <path d="#" />
             </svg>
           </div>
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto space-x-10">
-            <div class=" text-sm lg:flex-grow space-x-20">
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto space-x-10">
+            <div className=" text-sm lg:flex-grow space-x-20">
               <a
                 href="/"
-                class="block mt-4 lg:inline-block text-4xl lg:mt-0 text-teal-200 hover:text-white mr-4"
+                className="block mt-4 lg:inline-block text-4xl lg:mt-0 text-teal-200 hover:text-white mr-4"
               >
                 Home
               </a>
               <a
                 href="#"
-                class="block mt-4 lg:inline-block text-4xl lg:mt-0 text-teal-200 hover:text-white mr-4"
+                className="block mt-4 lg:inline-block text-4xl lg:mt-0 text-teal-200 hover:text-white mr-4"
               >
                 Shopping Cart
               </a>
               <a
                 href="#"
-                class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
               ></a>
             </div>
             <div></div>
