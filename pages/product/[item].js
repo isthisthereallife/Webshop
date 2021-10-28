9f92ca78dff7bf68d3d95dc13f159e904b29b48e
JonasHolmkvist <jonas_holmkvist@hotmail.se>
2021-10-28 11:11:33 +0200

    Add quantity to cart

--- pages/product/[item].js
+++ pages/product/[item].js
@@ -1,23 +1,24 @@
-import { useState, useContext } from "react"
-import Link from "next/link"
-import Image from "next/image"
-import styles from "../../styles/[item].module.css"
-import Button from "react-bootstrap/Button"
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Card from "react-bootstrap/Card"
-import ListGroup from "react-bootstrap/ListGroup"
-import ListGroupItem from "react-bootstrap/ListGroupItem"
-import Accordion from "react-bootstrap/Accordion"
-import Navbar from "../../components/Navbar"
-import Footer from "../../components/Footer"
-import Cart from "../cart"
-import { CartItemContext } from "../_app"
+import { useState, useContext, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../styles/[item].module.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Accordion from "react-bootstrap/Accordion";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Cart from "../cart";
+import { CartItemContext } from "../_app";
 
 export default function Post({ product }) {
-  const [quantity, setQuantity] = useState(1)
-  const [cartItems, setCartItems] = useContext(CartItemContext)
+  const [quantity, setQuantity] = useState(1);
+  const [cartItems, setCartItems] = useContext(CartItemContext);
+  const quantityRef = useRef();
 
   return (
     <div className={styles.container}>
@@ -71,45 +72,51 @@ export default function Post({ product }) {
                 as={Col}
                 className={styles.buttonAddToCart}
                 onClick={() => {
+                  //setQuantity(quantityRef.current.value.valueOf());
                   setCartItems((prevCartItems) => [
                     ...prevCartItems,
                     {
+                      prodId: product.id,
                       prodName: product[0].name,
-                      prodPrice: product[0].ibu * quantity,
-                      q: quantity,
+                      prodPrice:
+                        product[0].ibu * quantityRef.current.value.valueOf(),
+                      q: quantityRef.current.value.valueOf(),
                     },
-                  ])
+                  ]);
 
-                  console.log(cartItems)
+                  console.log(cartItems);
                 }}
               >
                 {" "}
                 Add to cart
               </Button>
+              <p>Qantity</p>
+
+              <input ref={quantityRef} type="number" placeholder="1"></input>
             </Row>
           </Card.Body>
         </div>
         <Footer />
       </main>
     </div>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const res = await fetch("https://api.punkapi.com/v2/beers?page1&per_page=80")
-  const data = await res.json()
+  const res = await fetch("https://api.punkapi.com/v2/beers?page1&per_page=80");
+  const data = await res.json();
 
   const paths = data.map((product) => ({
     params: { item: `${product.id.toString()}` },
-  }))
+  }));
 
-  console.log("paths", paths)
+  console.log("paths", paths);
 
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const res = await fetch(`https://api.punkapi.com/v2/beers/${params.item}`)
-  const data = await res.json()
-  return { props: { product: data } }
+  const res = await fetch(`https://api.punkapi.com/v2/beers/${params.item}`);
+  const data = await res.json();
+  return { props: { product: data } };
 }
