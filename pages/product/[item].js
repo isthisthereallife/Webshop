59d89a688324c78ca79790ff8f967b5934579bf3
Magnus <isthisthereallife@riseup.net>
2021-10-28 15:25:24 +0200

    fallback i [item|getStaticPaths ska vara true

--- pages/product/[item].js
+++ pages/product/[item].js
@@ -1,24 +1,24 @@
-import { useState, useContext, useRef } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import styles from "../../styles/[item].module.css";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
-import Accordion from "react-bootstrap/Accordion";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
-import Cart from "../cart";
-import { CartItemContext } from "../_app";
+import { useState, useContext, useRef } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import styles from "../../styles/[item].module.css"
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+import ListGroup from "react-bootstrap/ListGroup"
+import ListGroupItem from "react-bootstrap/ListGroupItem"
+import Accordion from "react-bootstrap/Accordion"
+import Navbar from "../../components/Navbar"
+import Footer from "../../components/Footer"
+import Cart from "../cart"
+import { CartItemContext } from "../_app"
 
 export default function Post({ product }) {
-  const [quantity, setQuantity] = useState(1);
-  const [cartItems, setCartItems] = useContext(CartItemContext);
-  const quantityRef = useRef();
+  const [quantity, setQuantity] = useState(1)
+  const [cartItems, setCartItems] = useContext(CartItemContext)
+  const quantityRef = useRef()
 
   return (
     <div className={styles.container}>
@@ -66,7 +66,7 @@ export default function Post({ product }) {
                 <Button className={styles.buttonBack} as={Col}>
                   Go Back
                 </Button>
-              </Link>
+              </Link>true
               <Button
                 as={Col}
                 className={styles.buttonAddToCart}
@@ -81,9 +81,9 @@ export default function Post({ product }) {
                         product[0].ibu * quantityRef.current.value.valueOf(),
                       q: quantityRef.current.value.valueOf(),
                     },
-                  ]);
+                  ])
 
-                  console.log(cartItems);
+                  console.log(cartItems)
                 }}
               >
                 {" "}
@@ -101,20 +101,20 @@ export default function Post({ product }) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch("https://api.punkapi.com/v2/beers?page1&per_page=80");
-  const data = await res.json();
+  const res = await fetch("https://api.punkapi.com/v2/beers?page1&per_page=80")
+  const data = await res.json()
 
   const paths = data.map((product) => ({
     params: { item: `${product.id.toString()}` },
-  }));
+  }))
 
-  console.log("paths", paths);
+  console.log("paths", paths)
 
-  return { paths, fallback: false };
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps({ params }) {
-  const res = await fetch(`https://api.punkapi.com/v2/beers/${params.item}`);
-  const data = await res.json();
-  return { props: { product: data } };
+  const res = await fetch(`https://api.punkapi.com/v2/beers/${params.item}`)
+  const data = await res.json()
+  return { props: { product: data } }
 }
