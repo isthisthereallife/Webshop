18722eef7412c7a2cb6e14bdbe0a446971e668c3
Magnus <isthisthereallife@riseup.net>
2021-10-28 10:58:23 +0200

    simplify

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,52 +1,13 @@
 import Link from "next/link"
+import styles from '../styles/Navbar.module.css'
+export default function Navbar() {
 
-function Navbar() {
   return (
-    <>
-      <div className="navContainer">
-        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-16">
-          <div className="flex items-center flex-shrink-0 text-white mr-12 space-x-18">
-            <svg
-              className="fill-current h-12 w-8 mr-9"
-              width="54"
-              height="54"
-              viewBox="0 0 54 54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="#" />
-            </svg>
+    <div className={styles.navbar}>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/checkout"><a>Cart</a></Link>
 
-            <span className="font-semibold text-6xl tracking-tight">
-              Brewdog Beer
-            </span>
-          </div>
-          <div className="block lg:hidden">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="#" />
-            </svg>
-          </div>
-          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto space-x-10">
-            <div className=" text-sm lg:flex-grow space-x-20">
-              <Link
-                href="/"
-              >
-                Home
-              </Link>
-              <Link
-                href="/cart"
-              >Shopping Cart
-              </Link>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </>
+    </div >
+
   )
 }
-
-export default Navbar
